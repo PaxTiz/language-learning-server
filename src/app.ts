68e6b6df9516b73605b27f503fe536b130b13da7
Valentin Cernuta <vcernuta3@gmail.com>
2022-06-21 00:06:32 +0200

    fix: app.ts path

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import 'express-async-errors'
 import { join } from 'path'
-import Application from './src/application'
+import Application from './application'
 
 new Application({
     env: join(__dirname, '.env'),
